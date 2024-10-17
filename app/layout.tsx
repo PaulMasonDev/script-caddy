adde192e5e2c7d90dfc9bd65b044e98f2748000f
Paul Mason <webdevpaulmason@gmail.com>
2024-10-17 14:52:53 -0700

    Safe area

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen max-sm:flex-col-reverse overflow-hidden justify-between`}
+          style={{ paddingBottom: "env(safe-area-inset-bottom)" }}
         >
           <NavBar />
           {children}
